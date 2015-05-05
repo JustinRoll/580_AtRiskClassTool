dbe00ca9170c8d5652514440b0e84274356c98ba
James Bilous <james.bilous@gmail.com>
2015-05-05 12:06:44 -0700

    Added mapping functionality from jira issues to git commits

--- Mapper.py
+++ Mapper.py
@@ -5,56 +5,73 @@ class Mapper(object):
 
     def mapCommitsToTickets(self, commits, tickets, token):
 
+        orphanedCommits = 0
+        orphanedTickets = [0]
+
         #Hash ticket objects to their IDs
-        ticketHash = self.hashTickets(tickets)
+        ticketHash = self.hashTickets(tickets, orphanedTickets)
 
         #Tickets hashed to -> list of commits
-        ticketsToCommits = {}
+        ticketsToCommits = dict()
 
         for commit in commits:
             commitNumber = self.parseCommitNumber(commit, token)
 
+            ticket = None
+
             # Skip if commit number missing
-            if commitNumber < 0:
+            if commitNumber is None:
+                orphanedCommits += 1
                 continue
 
             #find the ticket by the issue ID in the commit
             if commitNumber in ticketHash:
-                ticketHash[commitNumber] = commit;
-
-            ticket = ticketHash[commitNumber]
+                ticket = ticketHash[commitNumber];
+            else:
+                continue
 
             # Get the list of commits for that ticket if it exists
             # Create it if it does not
-            ticketsToCommits[ticket].append(commit)
+            if ticket in ticketsToCommits:
+                ticketsToCommits[ticket].append(commit)
+            else:
+                ticketsToCommits[ticket] = [commit]
+
+        print("Total Tickets: " + str(len(tickets)))
+        print("Orphaned Tickets: " + str(orphanedTickets[0]))
+        print("Total Commits: " + str(len(commits)))
+        print("Orphaned Commits: " + str(orphanedCommits))
 
         return ticketsToCommits
 
 
 
 
-    def hashTickets(self, tickets):
+    def hashTickets(self, tickets, orphanedTickets):
 
-        ticketHash = {}
+        ticketHash = dict()
+
         for ticket in tickets:
-            print(ticket.issueId);
-            ticketHash[ticket.issueId] = ticket
+            if ticket.issueId != "":
+                ticketHash[ticket.issueId.replace(" ", "")] = ticket
+            else:
+                orphanedTickets[0] += 1
 
         return ticketHash
 
     def parseCommitNumber(self, commit, token):
             # Find the token in the string
             commitMessage = commit.commit.message
-            index = commitMessage.find(token);
+            beginCommitIndex = commitMessage.find(token);
 
             # Parse out the commit #
-            if index >= 0:
-                beginCommitIndex = index + len(token)
+            if beginCommitIndex >= 0:
+                beginCommitIndex += len(token)
                 regex = "(?![0-9])"
 
                 m = re.search(regex, commitMessage[beginCommitIndex:])
                 endCommitIndex = m.start()
-                return int(commitMessage[beginCommitIndex:beginCommitIndex + endCommitIndex])
+                return commitMessage[beginCommitIndex - len(token):beginCommitIndex + endCommitIndex]
             else:
-                return -1
+                return None
 
--- dataExample.py
+++ dataExample.py
@@ -6,9 +6,8 @@ gitData = pickle.load(open("data/gitData.p", "rb"))
 jiraData = pickle.load(open("data/jiraData.p", "rb"))
 print(len(jiraData))
 
-#for comm in gitData:
-#    print(comm.commit.message)
-
 for issue in jiraData:
     print(issue.issueId)
+
+print("total: " + str(i))
 
